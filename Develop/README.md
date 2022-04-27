5d163993e0d006e1252f6032f033f49bfffe6e32
Aubree Alexander <aubree.alexander@gmail.com>
2022-04-27 18:33:38 -0500

    update renderLicenseSection function, update generateMarkdown function

---
+++ Develop/README.md
@@ -0,0 +1,31 @@
+# adslfj
+  ## Description
+  asdlfjj
+  ## Table of Contents
+  ### [Installation](#Installation)
+  ### [Usage](#Usage)
+  ### [License]
+  ### [Tests]
+  ### [Badges]
+  ### [Contributing]
+  ### [Questions]
+  ## Installation
+  asdflkjlsdkfj
+  ## Usage
+  adslfkj
+  ## License
+  
+  Here is the license:
+  ![License](https://img.shields.io/badge/License-APACHE 2.0-yellow.svg)
+  
+  ## Tests
+  The command that should be used to run tests is:
+  npm test
+  ## Badges
+  ## Contributing
+  sldfj
+  ## Questions
+  For additional questions please reach out to me at:
+  <aldsfj>
+  Or, view my other projects at:
+  (https://github.com/aaldksfj)
--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 
 // TODO: Create an array of questions for user input
@@ -29,32 +31,47 @@ const questions = [
     type: 'list',
     name: 'license',
     message: 'Please select a license.',
-    choices: ['Apache', 'Boost', 'BSD', 'Creative Commons', 'Eclipse', 'GNU', 'IBM', 'ISC', 'MIT', 'Mozilla', 'Perl', 'SIL', 'Unlicense', 'WTFPL', 'Zlib', 'None']
+    choices: ['APACHE 2.0', 'MIT', 'GPL 3.0', 'BSD 3', 'None']
 },
 {
     type: 'input',
     name: 'contributing',
     message: 'Please describe how others can contribute to this project.'
+},
+{
+    type: 'input',
+    name: 'test',
+    message: 'What command should run to run tests?',
+    default: 'npm test'
+},
+{
+    type: 'input',
+    name: 'github',
+    message: 'Please enter your Github username.'
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'Please enter your email address.'
 }
-//  Contributing, Tests, and Questions
 
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    //AA - question - why isn't this syntax listed on inquirer npm website? Found this in a google search.
+    
     inquirer.prompt(questions)
     .then(function(response) {
-        //AA - question - is this correct? specifically generateMarkdown?
-        return fs.writeFileSync('README.md', generateMarkdown, function(err) {
+        
+        return fs.writeFileSync('README.md', generateMarkdown(response), function(err) {
             if (err) {
                 throw err;
             };
         });
-     
+    })
 }
 
 // Function call to initialize app
